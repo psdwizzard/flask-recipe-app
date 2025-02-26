f2d2ec45c904ec46d9e046bdae91398d7c3b4554
Kenneth Loebenberg <kloebenberg@gmail.com>
2025-02-26 08:37:36 -0500

    Update start_flask.bat

--- start_flask.bat
+++ start_flask.bat
@@ -1,7 +1,5 @@
 @echo off
-cd /d C:\Users\joelx\Desktop\Recipe_app
-echo Starting Flask app...
-start cmd /k "python app.py"
-timeout /t 5
+call venv\Scripts\activate
+python app.py
 echo Opening Force Sync in browser...
 start "" http://127.0.0.1:5000/force_sync
